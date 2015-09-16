08bb542a3bb4d4dc7309bdef5b2da1ea08d84a59
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2015-09-15 19:06:57 -0500

    Ejemplos de la semana 11

---
+++ semana11/Threads/ThreadParametersExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread thread1 = new Thread(CountTo100);
+			thread1.Start("Thread1");
+			Thread thread2 = new Thread(CountTo100);
+			thread2.Start("Thread2");
+			thread1.Join();
+			thread2.Join();
+			Console.ReadKey();
+		}
+
+		public static void CountTo100(object data)
+		{
+			string threadName = (string)data;
+			for (int index = 0; index < 100; index++)
+				Console.WriteLine(threadName + ": " + (index + 1));
+		}
+	}
+}
---
+++ semana11/Threads/ThreadParametersExample2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using System.Threading.Tasks;
+namespace ThreadParametersExample2
+{
+	public class DivisionProblem
+	{
+		public double Dividend { get; set; } // the top
+		public double Divisor { get; set; } // the bottom
+		public double Quotient { get; set; } // the result (normally would be returned)
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread thread = new Thread(Divide);
+			DivisionProblem problem = new DivisionProblem();
+			problem.Dividend = 8;
+			problem.Divisor = 2;
+			thread.Start(problem);
+			thread.Join();
+			Console.WriteLine("Result: " + problem.Quotient);
+			Console.ReadKey();
+		}
+		public static void Divide(object input)
+		{
+			DivisionProblem problem = (DivisionProblem)input;
+			problem.Quotient = problem.Dividend / problem.Divisor;
+		}
+	}
+}
