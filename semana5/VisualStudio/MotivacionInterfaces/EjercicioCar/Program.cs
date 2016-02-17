6c2ee6175a03253ee6dd4d1257c9b665ed9e8b67
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-02-17 06:42:37 -0500

    Examples

---
+++ semana5/VisualStudio/MotivacionInterfaces/EjercicioCar/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCar
+{
+
+    public class Car : IEquatable<Car>
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        // Implementation of IEquatable<T> interface
+        public bool Equals(Car car)
+        {
+            if (this.Make == car.Make &&
+                this.Model == car.Model &&
+                this.Year == car.Year)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Fiesta";
+            car1.Year = "2015";
+
+            Car car2 = new Car();
+            car2.Make = "Chevrolet";
+            car2.Model = "Spark GT";
+            car2.Year = "2014";
+
+            Car car3 = new Car();
+            car3.Make = "Ford";
+            car3.Model = "Fiesta";
+            car3.Year = "2015";
+
+
+            if (car1.Equals(car2))
+            {
+                Console.WriteLine("car1 is equal to car2");
+            }
+            else Console.WriteLine("car1 is not equal to car2");
+
+            if (car1.Equals(car3))
+            {
+                Console.WriteLine("car1 is equal to car3");
+            }
+            else Console.WriteLine("car1 is not equal to car3");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ semana5/VisualStudio/MotivacionInterfaces/MotivacionInterfaces/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotivacionGenerics
+{
+
+    public class ListOfNumbers
+    {
+
+        private int[] numbers;
+
+        public ListOfNumbers()
+        {
+
+            numbers = new int[0];
+
+        }
+
+        public void AddNumber(int newNumber)
+        {
+            // Add some code in here to make a new array, slightly larger 
+            // than it already was, and add your number in. 
+        }
+
+        public int GetNumber(int index)
+        {
+            return numbers[index];
+        }
+    }
+
+
+    public class ListOfStrings
+    {
+
+        private string[] strings;
+
+        public ListOfStrings()
+        {
+
+            strings = new string[0];
+
+        }
+
+        public void AddString(string newString)
+        {
+            // Add some code in here to make a new array, slightly larger 
+            // than it already was, and add your number in. 
+        }
+
+        public string GetString(int index)
+        {
+            return strings[index];
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+}
