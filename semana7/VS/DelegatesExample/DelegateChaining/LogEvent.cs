7784bb7385d152a8958a8ac5d3c36a7ce8d73f3e
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-03-02 09:19:44 -0500

    Delegate chaining

---
+++ semana7/VS/DelegatesExample/DelegateChaining/LogEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace DelegateChaining
+{
+    public class LogEvent
+    {
+        public string Text { get; set; }
+        public LogEvent(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ semana7/VS/DelegatesExample/DelegateChaining/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DelegateChaining
+{
+    public delegate void LogEventHandler(LogEvent logEvent);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogEventHandler logHandlers = LogToConsole;
+            logHandlers += LogToFile;
+            logHandlers(new LogEvent("Message"));
+            logHandlers -= LogToFile;
+            logHandlers(new LogEvent("Message2"));
+
+
+            Console.ReadKey();
+        }
+      
+        private static void LogToConsole(LogEvent logEvent)
+        {
+            Console.WriteLine(logEvent.Text);
+        }
+
+        private static void LogToFile(LogEvent logEvent)
+        {
+            File.AppendAllText("log.txt", logEvent.Text + Environment.NewLine);
+        }
+    }
+
+}
