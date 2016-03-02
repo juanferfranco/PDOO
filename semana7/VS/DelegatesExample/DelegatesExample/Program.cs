3c17db27d2e8663ce9e7dc81ed0e905c41f3a113
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-03-02 08:30:24 -0500

    Delegate Example

---
+++ semana7/VS/DelegatesExample/DelegatesExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+
+    public delegate int MathDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathDelegate mathOperation = Add;
+            int a = 5; int b = 7;
+            int result = mathOperation(a, b);
+
+            Console.WriteLine("result: {0}", result);
+            Console.ReadKey();
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Power(int baseNumber, int exponent)
+        {
+            return (int)Math.Pow(baseNumber, exponent);
+        }
+    }
+}
