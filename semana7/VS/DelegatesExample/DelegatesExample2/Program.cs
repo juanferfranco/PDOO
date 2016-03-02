2d84dc2dab8f1b76b2d2551d46a0493b3da6c592
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-03-02 08:37:00 -0500

    Delegates example 2

--- semana7/VS/DelegatesExample/DelegatesExample2/Program.cs
+++ semana7/VS/DelegatesExample/DelegatesExample2/Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DelegatesExample
+namespace DelegatesExample2
 {
-
     public delegate int MathDelegate(int a, int b);
 
     class Program
     {
         static void Main(string[] args)
         {
-            MathDelegate mathOperation = Add;
-            int a = 5; int b = 7;
-            int result = mathOperation(a, b);
+            MathDelegate[] mathOperationArray = { Add, Subtract, Power };
+            int a = 5;
+            int b = 7;
+            //int result;
+            foreach (MathDelegate op in mathOperationArray)
+            {
+                a = 5;
+                b = 7;
+                int result = op(a, b);
+                Console.WriteLine("result: {0}", result);
+            }
 
-            Console.WriteLine("result: {0}", result);
             Console.ReadKey();
         }
 
