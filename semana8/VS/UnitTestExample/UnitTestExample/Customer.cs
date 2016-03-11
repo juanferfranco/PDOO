0f5e4f488d69abb4476265109b4bd8dfe74a4089
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-03-11 14:37:26 -0500

    UnitTest example

---
+++ semana8/VS/UnitTestExample/UnitTestExample/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample
+{
+    public class Customer
+    {
+        public string lastName { get; set; }
+
+        public string firstName { get; set; }
+
+        public string fullName {
+            get
+            {
+                return firstName + ',' + lastName;
+            }
+        }
+    }
+}
---
+++ semana8/VS/UnitTestExample/UnitTestProject1/CustomerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestExample;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FirstNameTest()
+        {
+        //--Arrange
+        Customer customer = new Customer();
+        customer.firstName = "Sara";
+        customer.lastName = "Franco";
+
+
+        string expected = "Sara,Franco";
+
+        //--Act
+
+        string actual = customer.fullName;
+
+        //--Assert
+        Assert.AreEqual(expected, actual);
+        }
+    }
+}
