2bc3e4a4c50ce6b462fd94454ab400d7acecafef
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2016-04-13 08:24:21 -0500

    semana 12 2016-10

---
+++ semana12/2016-10/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour 
+{
+	//Scene ID to change to
+	public int DestScene = 0;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Press N to move to next scene
+		if(Input.GetKeyDown(KeyCode.N))
+		{
+			Application.LoadLevel(DestScene);
+		}
+	}
+}
